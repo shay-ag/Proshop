3f2d8179f388cbcf8790134dae5fed3415457f00
Shashwat Agrawal <shay.ag13200@gmail.com>
2021-11-12 00:20:21 +0530

    implemented user auth at backend

--- backend/server.js
+++ backend/server.js
@@ -5,15 +5,20 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 // import products from '../backend/data/products.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
 const app = Express();
 
+app.use(Express.json()); //allow to accept json data from body
+
 app.get('/', (req, res) => {
     res.send('API is running....');
 });
+
+app.use('/api/users', userRoutes);
 
 app.use('/api/products', productRoutes);
 
@@ -22,5 +27,4 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
